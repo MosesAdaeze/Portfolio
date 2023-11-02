10e02a6de80ff537b2fb03223dc8be20cace8949
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-11-02 15:27:16 +0100

    js

--- script.js
+++ script.js
@@ -27,9 +27,116 @@ class DragScroll {
     }
 
     init() {
-        this.progress - 0
+        this.progress = 0;
+        this.speed = 0;
+        this.oldX = 0;
+        this.x = 0;
+        this.playrate = 0;
+
+        this.binding();
+        this.events();
+        this.calculate();
+        this.raf();
+    }
+
+    bindings() {
+        [
+            "events",
+            "calculate",
+            "raf",
+            "handleWheel",
+            "move",
+            "handleTouchStart",
+            "handleTouchMove",
+            "handleTouchEnd",
+        ].forEach((method) => {
+            this[method] = this[method].bind(this);
+        });
+    }
+
+    calculate() {
+        this.progress = 0;
+        this.wrapWidth = this.items[0].clientWidth * this,
+        items.length;
+        this.wrap.style.width = '${this.wrapWidth}px';
+        this.maxScroll = this.wrapWidth - this.el.clientWidth;
+    }
+
+
+    handleWheel(e) {
+        this.progress += e.deltaY;
+        this.move();
+    }
+
+    handleTouchStart(e) {
+        e.preventDefault();
+        this.dragging = true;
+        this.startX = e.clientX || e.touches[0].clientX;
+    }
+
+    handleTOuchMove(e) {
+        if(!this.dragging) return false;
+        const x = e.clientX || .touches[0].clientX;
+        this.progress += (this.startX - x) * 2.5;
+        this.startX = x;
+        this.move();
+    }
+
+    handleTOuchEnd() {
+        this.dragging = false;
+    }
+
+    move() {
+        this.progress = clamp(this.progress, 0, this.maxScroll);
+    }
+
+    events() {
+        window.addEventListener("reset", this.calculate);
+        window.addEventListener("wheel",this.handelWheel);
+
+        this.el.addEventListener("touchstart", this.handleTouchStart);
+        window.addEventListener("touchmove", this.handleTouchMove);
+        window.addEventListener("touchend", this.handleTouchEnd);
+
+        window.addEventListener("mousedown", this.handleTouchStart);
+        window.addEventListener("mousemove", this.handleMove);
+        window.addEventListener("mouseup", this.handleTouchEnd);
+        document.body.addEventListener("mouseleave", this.handleTouchEnd);
+    }
+
+
+    raf() {
+        this.x = lerp(this.x, this.progress, 0.1);
+        this.playrate = this.x / this.maxScroll;
+
+        this.wrap.style.transform = 'translateX(${-this.x}px)';
+        this.bar.style.transform = 'scaleX(${0.18 + this.playrate * 0.82})';
+        this.speed = Math.min(100, this.oldX - this.x);
+        this.oldX = this.x;
+
+        this.scale = lerp(this.scale, this.speed, 0.1);
+        this.items.forEach((item) => {
+            item.style.transform = 'scale(${1 - Math.abs(this.speed) * 0.005})';
+            item.querySelector("img").style.transform = 'scaleX(${1 + Math.abs(this.speed) * 0.004})';
+        });
     }
 }
+
+const scroll = new DragScroll({
+    el: ".slider",
+    wrap: ".slider_wrapper",
+    item: ".slider-item",
+    bar: ".slider-progress-bar",
+});
+
+
+const animateScroll = () => {
+    requestAnimationFrame(animateScroll);
+    scroll.raf();
+};
+
+animateScroll();
+
 
 
 
