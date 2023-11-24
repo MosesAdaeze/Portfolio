c4e7f9e0d07a6556c3056716f752ceb63c9b6a25
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-11-24 22:13:43 +0100

    .activeClass
    committed

--- script.js
+++ script.js
@@ -16,6 +16,33 @@ menu_item.forEach(item=>{
 });
 // Nav bar menu .
 
+//////////////// ACTIVE CLASS 
+let homeHeader = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    homeHeader.classList.toggle('shadow', window.scrollY > 0);
+});
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .nav li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .nav li a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
+
 // IMAGE SLIDER - RECENT PROJECT SECTION 2
 let sidebar = document.getElementsByClassName("sidebar")[0];
 let sidebar_content = document.getElementsByClassName("content_wrapper")[0];
